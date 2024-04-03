f0ea7a29aff74f49be4d00b888a772890feebb0c
JackyZhang1219 <zhangzhengxi1219@gmail.com>
2024-04-03 16:26:32 -0500

    firebase config

---
+++ frontend/firebase/firebase.tsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCmDSB9YdqFKiu-CtfmPDhGmyJOdi9viB8",
+  authDomain: "savor-4dedb.firebaseapp.com",
+  databaseURL: "https://savor-4dedb-default-rtdb.firebaseio.com",
+  projectId: "savor-4dedb",
+  storageBucket: "savor-4dedb.appspot.com",
+  messagingSenderId: "6003583577",
+  appId: "1:6003583577:web:fc854a328fc6800a379f48",
+  measurementId: "G-FCM14JSJCN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
