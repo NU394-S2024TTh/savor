7e5624a69a54b10bf65dcd1378d95aeed5e7a0fe
elliasc04 <guoyethan@gmail.com>
2024-04-02 23:30:48 -0500

    revert template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,60 @@ import React, { useState } from 'react';
 
 import logo from './logo.svg';
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="header">
+          {' '}
+          🚀 Vite + React + Typescript + Vitest 🤘 & <br />
+          Eslint 🔥+ Prettier
+        </p>
+
+        <div className="body">
+          {' '}
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
+          <p>
+            Mess up the code in <code>App.tsx </code> and save the file.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitest.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vitest Docs
+            </a>
+          </p>
+        </div>
+      </header>
+    </div>
+  );
 }
+
+export default App;
