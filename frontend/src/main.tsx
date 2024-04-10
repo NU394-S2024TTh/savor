8683a1debe7568495cd56d7be240c0b0cf661082
Alan Wang <alanwang2026@u.northwestern.edu>
2024-04-09 21:07:19 -0500

    Co-authored-by: Zhengxi Zhang <JackyZhang1219@users.noreply.github.com> Co-authored-by: BohengZhao <BohengZhao@users.noreply.github.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,9 +6,10 @@ import ReactDOM from 'react-dom/client';
 
 import Homepage from './homepage/homepage';
 
+import LoadingPage from './uploadpage/LoadingPage';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Homepage />
+  <Homepage /> 
   </React.StrictMode>,
 );
