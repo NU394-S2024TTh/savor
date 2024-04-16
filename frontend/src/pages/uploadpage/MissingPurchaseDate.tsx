1d730a0b0067e2bd0cdfee83e529097b21072f48
JackyZhang1219 <zhangzhengxi1219@gmail.com>
2024-04-16 02:40:54 -0500

    Fix missing purchase date styling

--- frontend/src/pages/uploadpage/MissingPurchaseDate.tsx
+++ frontend/src/pages/uploadpage/MissingPurchaseDate.tsx
@@ -12,19 +12,24 @@ export default function MissingPurchaseDate({ setResponse, setNoPurchaseDate }: 
 	};
 
 	return (
-		<div className="flex h-screen flex-col items-center justify-center">
-			<div className="text-3xl font-bold">No purchase date found in the receipt ☹️</div>
-			<div className="text-2xl font-bold">Select the date below</div>
-			<input
-				name="purchase-date"
-				type="date"
-				className="rounded-2xl bg-green-500 px-20 py-4"
-				value={purchaseDate.toString()}
-				onChange={(e) => setPurchaseDate(e.target.value)}
-			/>
+		<div className="flex h-screen flex-col items-center">
+			<div className="h-1/4 HomePageTitle mt-6"> Confirm Items </div>
+			<div className="flex h-1/2 flex-col items-center justify-center">
+				<div className="mb-8 text-xl text-black">No purchase date found in the receipt ☹️</div>
+				<div className="mb-4 text-lg text-black">Please select a date below</div>
+				<input
+					name="purchase-date"
+					type="date"
+					className="mb-4 rounded-2xl py-4 text-black"
+					value={purchaseDate.toString()}
+					onChange={(e) => setPurchaseDate(e.target.value)}
+				/>
+			</div>
+			<div className="flex h-1/4 items-end mb-20">
 			<button className="rounded-2xl bg-green-500 px-20 py-4" onClick={onClick}>
-				Submit
+				Continue
 			</button>
+			</div>
 		</div>
 	);
 }
