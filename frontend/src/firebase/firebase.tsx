1cc8eab9ced455cb0b45909952b706ae80bc3b73
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-04-23 22:43:06 -0500

    fix some format errors and run lint

--- frontend/src/firebase/firebase.tsx
+++ frontend/src/firebase/firebase.tsx
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
